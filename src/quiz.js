5994cc53c9a5285f502bc76f5d2e9446f0b408f2
Johan Norman <johan@jnorman.se>
2018-01-18 22:36:32 +0100

    added animations, styling and quiz func

--- src/quiz.js
+++ src/quiz.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { TimelineMax, TweenMax, Elastic } from 'gsap';
 
 const trueFriends = [
   {id: 1, name: 'Johan', img: 'http://google.se'},
@@ -13,45 +14,111 @@ const trueFriends = [
 ];
 
 const questionsAndAnswers = [
-  {question: 'lorem ipsum dolor sit amet?', friendId: 1 },
-  {question: '2 lorem ipsum dolor sit amet?', friendId: 2 },
-  {question: '3 lorem ipsum dolor sit amet?', friendId: 4 },
-  {question: '4 lorem ipsum dolor sit amet?', friendId: 5 },
-  {question: '5 lorem ipsum dolor sit amet?', friendId: 1 },
+  {question: 'Vem har voltat med en bil?', friendId: 1 },
+  {question: 'Vem har varit en stjärna på minigolf?', friendId: 6 },
+  {question: 'Vem har jobbat som bagare?', friendId: 5 },
+  {question: 'Vem är känd med låten "Fucka ur"?', friendId: 4 },
+  {question: 'Vem fan bor i bagiz?', friendId: 2 },
   {question: '6 lorem ipsum dolor sit amet?', friendId: 9 },
   {question: '7 lorem ipsum dolor sit amet?', friendId: 1 },
-  {question: '8 lorem ipsum dolor sit amet?', friendId: 3 },
   {question: '9 lorem ipsum dolor sit amet?', friendId: 6 },
   {question: '10 lorem ipsum dolor sit amet?', friendId: 8 }
 ];
+
+class SuccessMessage extends React.Component {
+  render() {
+    return(
+      <div id="WinMessage" className={this.props.show ? "feedback-dialog show" : "feedback-dialog"}>
+        <img src="http://www4.pictures.zimbio.com/mp/zgWFptmpEKSl.jpg" />
+      </div>
+    )
+  }
+}
+
+class FailMessage extends React.Component{
+  render() {
+    return(
+      <div id="FailMessage" className={this.props.show ? "feedback-dialog show" : "feedback-dialog"}>
+        <img src="https://www.askideas.com/media/49/Athlete-Funny-Fail-Meme-Picture.jpg" />
+      </div>
+    )
+  }
+}
 
 class Quiz extends React.Component{
 
   constructor(props){
     super(props);
     this.state = {
-      currentQuestion: 0
+      currentQuestion: 0,
+      showWinMsg: false,
+      showFailMsg: false
     }
     this.onClickHandler = this.onClickHandler.bind(this);
-    //this.showCurrentQuestion = this.showCurrentQuestion.bind(this);
+    this.showNextQuestion = this.showNextQuestion.bind(this);
   }
 
-  onClickHandler(){
+  onClickHandler(element){
+
+    const correctAnswer = element.id === questionsAndAnswers[this.state.currentQuestion].friendId ? true : false;
+
+    if(correctAnswer){
+      this.setState({showWinMsg: true});
+      this.showNextQuestion();
+    } else {
+      this.setState({showFailMsg: true});
+    }
+
+  }
+
+  showNextQuestion() {
+    // Show Next question
     const newQuestion = this.state.currentQuestion + 1;
     if(newQuestion < questionsAndAnswers.length) {
       this.setState({currentQuestion: newQuestion});
     } else {
       this.setState({currentQuestion: 0});
     }
+
+    var tl = new TimelineMax({delay: 1});
+    tl
+      .from("h2", 0.5, {bottom:-200, autoAlpha:0}, "headline")
+      .set(".friends-list", {visibility:"visible"})
+      .staggerFrom(".friends-list li", 0.1, {scale:0.9, autoAlpha:0}, 0.075, "headline+=1");
+
+    tl.play();
+
   }
 
   get people() {
-    const friends = trueFriends.map((friend, i) =>
-      <li key={'friendID_' + friend.id}>{friend.name}</li>
+    const friends = trueFriends.map((friend, index) =>
+      <li onClick={() => this.onClickHandler(friend)} key={'friendID_' + friend.id}>{friend.name}</li>
     );
     return(
       <ul className="friends-list">{friends}</ul>
     );
+  }
+
+  componentDidMount() {
+    var tl = new TimelineMax();
+    tl
+      .from("h2", 0.5, {bottom:-200, autoAlpha:0}, "headline")
+      .set(".friends-list", {visibility:"visible"})
+      .staggerFrom(".friends-list li", 0.1, {scale:0.9, autoAlpha:0}, 0.15, "headline+=0.55");
+
+    tl.play();
+  }
+
+  componentDidUpdate(nextProps, nextState){
+
+    if(this.state.showWinMsg || this.state.showFailMsg) {
+
+      this.turnOffMessage = setTimeout(() => {
+          this.setState({showWinMsg: false, showFailMsg: false});
+      }, 1000);
+
+    }
+
   }
 
   render() {
@@ -59,17 +126,21 @@ class Quiz extends React.Component{
     const currentQuestion = questionsAndAnswers[this.state.currentQuestion];
 
     return(
+
       <div className="quiz-wrapper">
+
+        <SuccessMessage show={this.state.showWinMsg ? true : false} />
+        <FailMessage show={this.state.showFailMsg ? true : false} />
+
         <div className="quiz-question-container">
           <section className="quiz-question-innercontainer">
-            <h1>Quiz</h1>
+            <h1>Känner du dina True Friendz?</h1>
             <h2>{currentQuestion.question}</h2>
           </section>
         </div>
         <div className="quiz-choice-container">
           <section className="quiz-choice-innercontainer">
             {this.people}
-            <button onClick={this.onClickHandler}>Nästa fråga</button>
           </section>
         </div>
       </div>
