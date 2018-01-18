d8dd4e01bfefaffca2e7a5c3d28beca042252808
Johan Norman <johan@jnorman.se>
2018-01-18 16:02:26 +0100

    added styling

--- src/quiz.js
+++ src/quiz.js
@@ -12,26 +12,67 @@ const trueFriends = [
   {id: 9, name: 'Isak', img: 'http://google.se'}
 ];
 
+const questionsAndAnswers = [
+  {question: 'lorem ipsum dolor sit amet?', friendId: 1 },
+  {question: '2 lorem ipsum dolor sit amet?', friendId: 2 },
+  {question: '3 lorem ipsum dolor sit amet?', friendId: 4 },
+  {question: '4 lorem ipsum dolor sit amet?', friendId: 5 },
+  {question: '5 lorem ipsum dolor sit amet?', friendId: 1 },
+  {question: '6 lorem ipsum dolor sit amet?', friendId: 9 },
+  {question: '7 lorem ipsum dolor sit amet?', friendId: 1 },
+  {question: '8 lorem ipsum dolor sit amet?', friendId: 3 },
+  {question: '9 lorem ipsum dolor sit amet?', friendId: 6 },
+  {question: '10 lorem ipsum dolor sit amet?', friendId: 8 }
+];
+
 class Quiz extends React.Component{
 
   constructor(props){
     super(props);
+    this.state = {
+      currentQuestion: 0
+    }
+    this.onClickHandler = this.onClickHandler.bind(this);
+    //this.showCurrentQuestion = this.showCurrentQuestion.bind(this);
+  }
 
+  onClickHandler(){
+    const newQuestion = this.state.currentQuestion + 1;
+    if(newQuestion < questionsAndAnswers.length) {
+      this.setState({currentQuestion: newQuestion});
+    } else {
+      this.setState({currentQuestion: 0});
+    }
   }
 
   get people() {
-    //truFrends.map(friend => )
+    const friends = trueFriends.map((friend, i) =>
+      <li key={'friendID_' + friend.id}>{friend.name}</li>
+    );
     return(
-      <div>
-      lol
-      <div>
-    )
+      <ul className="friends-list">{friends}</ul>
+    );
   }
 
   render() {
+
+    const currentQuestion = questionsAndAnswers[this.state.currentQuestion];
+
     return(
-      <h1>Quiz</h1>
-      <h2>{this.people}</h2>
+      <div className="quiz-wrapper">
+        <div className="quiz-question-container">
+          <section className="quiz-question-innercontainer">
+            <h1>Quiz</h1>
+            <h2>{currentQuestion.question}</h2>
+          </section>
+        </div>
+        <div className="quiz-choice-container">
+          <section className="quiz-choice-innercontainer">
+            {this.people}
+            <button onClick={this.onClickHandler}>Nästa fråga</button>
+          </section>
+        </div>
+      </div>
     )
   }
 }
